2b04ef68a8761113ca2a4bf9f958039fd8dbac1c
Gatsby131 <gatsby13@hotmail.com>
2020-04-28 13:59:52 +0200

    Registro1

--- app/src/main/java/com/cev/blablapub/MainActivity.java
+++ app/src/main/java/com/cev/blablapub/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cev.blablapub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import android.widget.CompoundButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    public static final String EXTRA_MESSAGE = "com.cev.blablapub.RegisterActivity.MESSAGE";
 
     Button login;
     Button registro;
@@ -26,15 +28,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     EditText loginPassword;
     Button loginButton;
 
-    LinearLayout vistaRegistro;
-    EditText nombre;
-    EditText apellidos;
-    EditText edad;
-    RadioGroup sexo;
-    RadioButton hombre;
-    RadioButton mujer;
-    CheckBox siHaAceptadoPoliticaUso;
-    Button registrarme;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,15 +46,6 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         loginPassword = findViewById(R.id.loginPassword);
         loginButton = findViewById(R.id.loginButton);
 
-        vistaRegistro = findViewById(R.id.vistaRegistro);
-        nombre = findViewById(R.id.nombre);
-        apellidos = findViewById(R.id.apellidos);
-        edad = findViewById(R.id.edad);
-        sexo = findViewById(R.id.sexo);
-        siHaAceptadoPoliticaUso = findViewById(R.id.aceptarCondiciones);
-        registrarme = findViewById(R.id.registrarse);
-
-        siHaAceptadoPoliticaUso.setOnCheckedChangeListener(this);
     }
 
     public void abrirLogin(View vista) {
@@ -87,71 +72,9 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         login.setVisibility(View.GONE);
         registro.setVisibility(View.GONE);
 
-        vistaRegistro.setVisibility(View.VISIBLE);
-
-        nombre.setText("");
-        apellidos.setText("");
-        edad.setText("");
-        sexo.clearCheck();
-
-        siHaAceptadoPoliticaUso.setChecked(false);
-    }
-
-    /*
-    Oculta los campos de datos y visualiza la presentación
-     */
-    public void ocultaRegistro(View vista) {
-        imagenTarta.setVisibility(View.VISIBLE);
-        registro.setVisibility(View.VISIBLE);
-
-        vistaRegistro.setVisibility(View.GONE);
-    }
-
-
-    /*
-    Comprueba si todos los datos necesarios se han rellenado
-    y si es así muestra el Toast correspondiente y oculta los campos de datos
-    y visualiza la presntación para permitir otro registro
-     */
-    public void inscribirse(View vista) {
-        Toast toastKO = Toast.makeText(this, "Faltan datos. Por favor complete la información solicitada",
-                Toast.LENGTH_LONG);
-        Toast toastOK = Toast.makeText(this, "Gracias " + nombre.getText() + " ¡La inscripción se ha hecho con éxito!",
-                Toast.LENGTH_LONG);
-
-        Toast toastMenorEdad = Toast.makeText(this, "Los participantes deben ser mayores de edad",
-                Toast.LENGTH_LONG);
-
-        int edadConcursante = 0;
-        boolean edadOK = false;
-        if (edad.getText().length() > 0) {
-            edadConcursante = Integer.parseInt(edad.getText().toString());
-        }
-        if (edadConcursante >= 18) {
-            edadOK = true;
-        }
-
-        boolean todoOK = false;
-        boolean todoDatosOK = false;
-
-        todoDatosOK = (nombre.getText().length() > 0) && (apellidos.getText().length() > 0) && (edad.getText().length() > 0)
-                && (hombre.isChecked() || mujer.isChecked()) &&
-                ((siHaAceptadoPoliticaUso.isChecked()) ||
-                        (siHaAceptadoPoliticaUso.isChecked() == false));
-
-        todoOK = todoDatosOK && edadOK;
-        //Si tod o bien guardar
-        if (todoOK) {
-            toastOK.show();
-            ocultaRegistro(vista);
-        }
-        if (!todoDatosOK) {
-            toastKO.show();
-        }
-        if (todoDatosOK && !edadOK) {
-            toastMenorEdad.show();
-        }
-
+        Intent intent = new Intent(this,RegisterActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "message");
+        startActivity(intent);
     }
 
     @Override
---
+++ app/src/main/java/com/cev/blablapub/PetitionsPost.java
@@ -0,0 +1,53 @@
+package com.cev.blablapub;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class PetitionsPost extends AsyncTask<String,Void,String> {
+    Context context;
+
+    PetitionsPost(Context context){
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... parametros) {
+        String url = parametros[0];
+        String name = parametros[1];
+        String email = parametros[2];
+        String password = parametros[3];
+        String edad = parametros[4];
+        String sexo = parametros[5];
+        String resultado ="";
+        RequestBody body = new FormBody.Builder()
+                .add("name",name)
+                .add("email",email)
+                .add("password",password)
+                .add("edad",edad)
+                .add("sexo",sexo)
+                .build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(url).post(body).build();
+        try {
+            Response response = okHttpClient.newCall(request).execute();
+            resultado = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+    @Override
+    protected void onPostExecute(String resultado) {
+        Toast.makeText(context,resultado,Toast.LENGTH_LONG).show();
+    }
+}
