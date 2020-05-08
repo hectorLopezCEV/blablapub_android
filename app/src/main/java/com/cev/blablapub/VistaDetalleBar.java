d6268bf5bcf8bc0259d0d9dad105a333a35d6851
jakiviki <javikido40@gmail>
2020-05-08 18:36:01 +0200

    commit para canbiar de rama

---
+++ app/src/main/java/com/cev/blablapub/VistaDetalleBar.java
@@ -0,0 +1,14 @@
+package com.cev.blablapub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VistaDetalleBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_detalle_bar);
+    }
+}
